eb3a87477fdb037a48177cdb7fdefbd52d5b7b7d
LUZ <balon.basket.90@gmail.com>
2023-11-04 14:19:14 -0600

    [+] Update Home Screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { globalStyles } from '../styles/global';
 
+/*We can access to the navigation property because our father here is RootNavigator 
+  through the Stack*/
+  
+/**Another way is adding the useNavigation Hook from react-navigation/native*/
+
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { globalStyles } from '../styles/global';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Home () 
 {
+  const navigation = useNavigation();
+  
   return (
     <View style={globalStyles.screenContainer}>
         <Text style={globalStyles.title}>Home</Text>
+        <Button 
+          onPress={() => navigation.navigate("Settings")}
+          title="Settings"/>
     </View>
   )
 }
