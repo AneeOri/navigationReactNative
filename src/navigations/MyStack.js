ddaae41d2386c42e96565b0f8e2435d3fb9d0fc7
LUZ <balon.basket.90@gmail.com>
2023-11-04 16:22:32 -0600

    [+] update MyStack

--- src/navigations/MyStack.js
+++ src/navigations/MyStack.js
@@ -1,9 +1,38 @@
 /** El stack se pone dentro de un root navigator para que pueda acceder a nuestra stack */
 import { createStackNavigator } from "@react-navigation/stack";
+import { View, Text } from "react-native";
+import { Colors } from "../constants/colors";
 import Home from "../screens/Home";
 import Settings from "../screens/Settings";
+//import CustomHeader from "./CustomHeader";
 
 const HomeStack = createStackNavigator();
+
+function CustomHeader ({title}) {
+    return(
+     <View
+     style={{
+        height: 80,
+        width: '100%',
+        backgroundColor: Colors.secondary,
+        padding:10,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+      }}
+     >
+        <Text
+          style={{
+              fontSize:20,
+              fontWeight:'bold',
+              color: Colors.ligth,
+          }}
+        >
+        {title}
+        </Text>
+     </View>
+    );
+  }
+  
 
 export default function MyStack (){
     return (
@@ -12,14 +41,19 @@ export default function MyStack (){
         initialRouteName="Home"
         screenOptions={{
             headerShown:true,
-            title: 'something',
+           // title: 'something',
+           headerTitleAlign: "center",
+           presentation:'transparentModal',
+           gestureEnabled: true,
+           header:({ route}) => 
+             (<CustomHeader title={route.name}/>),
         }}
       >
         <HomeStack.Screen name="Home" component={Home}/>
         <HomeStack.Screen 
            name="Settings" 
            component={Settings}
-           options={{title: 'settings'}}
+           //options={{title: 'settings'}}
         />
       </HomeStack.Navigator>
     );
