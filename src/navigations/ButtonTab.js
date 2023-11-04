2d67b5c01515940d14c59f67937a7eb74f9f6486
LUZ <balon.basket.90@gmail.com>
2023-11-04 17:59:45 -0600

    [+] Create Button Tab

---
+++ src/navigations/ButtonTab.js
@@ -0,0 +1,20 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../screens/Home";
+import Profile from "../screens/Profile";
+
+const MyTab = createBottomTabNavigator();
+
+export default function MyButtonTab() {
+    return(
+        <MyTab.Navigator>
+            <MyTab.Screen
+              name="Home"
+              component={Home}
+            />
+             <MyTab.Screen
+              name="Profile"
+              component={Profile}
+            />
+        </MyTab.Navigator>
+    );
+}
