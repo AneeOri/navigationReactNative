a0b509600591e12047b0098e3480d1a45f701243
LUZ <balon.basket.90@gmail.com>
2023-11-04 18:01:09 -0600

    [+] Update Root Navigator
    Adding my ButtoTab to my Root Navigation Container

--- src/navigations/RootNavigator.js
+++ src/navigations/RootNavigator.js
@@ -1,10 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
 import MyStack from "./MyStack";
+import MyButtonTab from "./ButtonTab";
 
 export default function RootNavigator () {
  return(
     <NavigationContainer>
-        <MyStack/>
+        <MyButtonTab/>
     </NavigationContainer>
  );
 }
