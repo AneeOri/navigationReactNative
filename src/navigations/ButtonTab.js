90dcdb924ddff23ab7bfe99f4a4c9daa0870bc3a
LUZ <balon.basket.90@gmail.com>
2023-11-06 16:06:01 -0600

    [+] Update Button Tab Component
    Adding icons

--- src/navigations/ButtonTab.js
+++ src/navigations/ButtonTab.js
@@ -1,19 +1,33 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Colors } from "../constants/colors";
 
 const MyTab = createBottomTabNavigator();
 
 export default function MyButtonTab() {
     return(
-        <MyTab.Navigator>
+        <MyTab.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerTitleAlign: 'center',
+            tabBarActiveTintColor: Colors.secondary,
+          }}
+        >
             <MyTab.Screen
               name="Home"
               component={Home}
+              options={{
+                tabBarIcon: ({color}) => (<FontAwesome5 name="home" size={25} color={color} />),
+              }}
             />
              <MyTab.Screen
               name="Profile"
               component={Profile}
+              options={{
+                tabBarIcon: ({color}) => (<FontAwesome5 name="user" size={24} color={color} />),
+              }}
             />
         </MyTab.Navigator>
     );
