158b28c2e5253fb85423e3c8d806ff9de7d2b9cc
LUZ <balon.basket.90@gmail.com>
2023-11-08 17:16:59 -0600

    [+] Update Login
    Saving data user in login flow

--- src/screens/authScreens/Login.js
+++ src/screens/authScreens/Login.js
@@ -1,15 +1,31 @@
 import { View, Text } from "react-native";
+import { useState } from "react";
 import { globalStyles } from "../../styles/global";
 import MyInput from "../../components/MyInput";
 import MyButton from "../../components/MyButton";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useDispatch } from "react-redux";
+import { signIn } from "../../features/auth/auth";
 
 export default function Login({ navigation}){
+    const [token, setToken] = useState('');
+    const dispatch = useDispatch();
+
+    async function save (value) {
+       try {
+           await AsyncStorage.setItem('@token' , value);
+           dispatch(signIn(value));
+           console.log('dataSaved!');
+       } catch (error) {
+          console.log(error);
+       }
+    }
     return(
         <View style={globalStyles.screenContainer}>
-            <Text style={globalStyles.title}> Sign Up</Text>
-            <MyInput label={'Email'}/>
+            <Text style={globalStyles.title}> Login</Text>
+            <MyInput label={'Email'} value={token} onChangeText={setToken}/>
             <MyInput label={'Password'} secureTextEntry/>
-            <MyButton title={'Sign In'}/>
+            <MyButton title={'Sign In'} onPress={() => save(token)}/>
             <MyButton title={'Sign Up'} onPress={() => navigation.navigate('SignUp')}/>
         </View>
     );
