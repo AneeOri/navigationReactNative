6a10f3e43605f56bc5571b868f3768db6141b191
LUZ <balon.basket.90@gmail.com>
2023-11-08 17:17:42 -0600

    [+] Update Profile
    Adding a signOut Button and removing item from storage

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
+import { useDispatch } from 'react-redux';
+import { signOut } from '../features/auth/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function Profile () 
-{
+export default function Profile (){
+  const dispatch = useDispatch();
+
   return (
     <View style={globalStyles.screenContainer}>
       <Text style={globalStyles.title}>Profile</Text>
+      <Button 
+        title='sign out' 
+        onPress={ async () => { 
+          await AsyncStorage.removeItem('@token');
+          dispatch(signOut())
+        }}
+      />
     </View>
   )
 }
