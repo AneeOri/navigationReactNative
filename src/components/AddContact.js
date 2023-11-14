9118bf65bfb2accb5bbfbdc56cf9b4972847bc94
LUZ <balon.basket.90@gmail.com>
2023-11-13 21:38:12 -0600

    [+] Update AddContact Component

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,19 +1,27 @@
 import { useState } from "react";
 import { View, Button , StyleSheet } from "react-native";
 import MyInput from "./MyInput";
+import { Colors } from "../constants/colors";
 
 export default function AddContact(){
     const [name, setName] = useState('');
 
     return(
-       <View>
-         <MyInput/>
+       <View style={styles.container}>
+         <View style={{width:'80%'}}>
+            <MyInput/>
+         </View>
+         <Button title="Add" color={Colors.primary}/>
        </View>
     );
 }
 
 const styles = StyleSheet.create({
   container:{
-
+      flexDirection:'row',
+      justifyContent:'center',
+      alignItems:'center',
+      width:'100%',
+      paddingVertical:'5%',
   },
 });
