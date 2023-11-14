60182edab85a33119dc07138dd14d605ea5ea215
LUZ <balon.basket.90@gmail.com>
2023-11-13 21:31:06 -0600

    [+] Update Contacts Screen

--- src/screens/Contacts.js
+++ src/screens/Contacts.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
+import AddContact from '../components/AddContact';
 
 export default function Contacts () 
 {
   return (
     <View style={globalStyles.screenContainer}>
-      <Text style={globalStyles.title}>Contacts</Text>
+      <AddContact/>
     </View>
   )
 }
