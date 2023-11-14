cf3ef00ea0d1e5cfefb7c26cd07b93463c31a112
LUZ <balon.basket.90@gmail.com>
2023-11-13 21:46:56 -0600

    [+] Update Contacts Screen

--- src/screens/Contacts.js
+++ src/screens/Contacts.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
 import AddContact from '../components/AddContact';
 
-export default function Contacts () 
-{
+export default function Contacts (){
+  const [contacts, setContacts] = useState(initialContacts);
+
   return (
     <View style={globalStyles.simpleContainer}>
       <AddContact/>
     </View>
   )
 }
+
+const initialContacts = [
+  {id:0, name:'Sara Lee'},
+  {id:1, name:'Kike Spe'},
+  {id:2, name:'Vero Espa'},
+]
