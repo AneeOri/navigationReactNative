0c9b0632951dfe8beaac69323a6b647abc139e0f
LUZ <balon.basket.90@gmail.com>
2023-11-18 20:03:16 -0600

    [+] Update Contacts Screen

--- src/screens/Contacts.js
+++ src/screens/Contacts.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { globalStyles } from '../styles/global';
 import AddContact from '../components/AddContact';
 
