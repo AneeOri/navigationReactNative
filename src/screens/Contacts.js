5ae72615e281598f6a2e86af3a250552a1b5962a
LUZ <balon.basket.90@gmail.com>
2023-11-20 17:44:38 -0600

    [+] Update Contacts Screen
    adding Handle Add, Delete and Change contact functions

--- src/screens/Contacts.js
+++ src/screens/Contacts.js
@@ -7,6 +7,16 @@ import ContactList from '../components/ContactList';
 export default function Contacts (){
   const [contacts, setContacts] = useState(initialContacts);
 
+  function handleAddContact(name){
+    setContacts([...contacts,{id:nextId++, name}]);
+  }
+  function handleDeleteContact(id){
+    setContacts(contacts.filter(contact => contact.id !== id));
+  }
+  function handleChangeContact(contact){
+    setContacts(contacts.map( c => (c.id === contact.id ? contact : c)));
+  }
+
   return (
     <View style={globalStyles.simpleContainer}>
       <AddContact/>
@@ -15,6 +25,7 @@ export default function Contacts (){
   )
 }
 
+let nextId = 3;
 const initialContacts = [
   {id:0, name:'Sara Lee'},
   {id:1, name:'Kike Spe'},
