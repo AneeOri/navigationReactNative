5b92ba1688f747456bc6b232457369a833053b57
LUZ <balon.basket.90@gmail.com>
2023-11-19 21:23:35 -0600

    [+] Update ContactList
    Initial design of the Contact List

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,5 +1,19 @@
 import { Button, ScrollView,View, Text, StyleSheet, Image,Pressable } from "react-native";
 
 export default function ContactList({contacts}){
+return(
+    <ScrollView>
+        {contacts.map((contact, index) => (
+            <Contact key={index} contact={contact.name}/>
+        ))}
+    </ScrollView>
+)
+}
 
+function Contact ({contact}){
+    return(
+        <View>
+         <Text>{contact}</Text>
+        </View>
+    );
 }
