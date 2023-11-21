d02c3f7ad7c21e45433fe4e08e2b264db7f15740
LUZ <balon.basket.90@gmail.com>
2023-11-20 21:13:45 -0600

    [+] Update Contact List
    Adding on change and on Delete events

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,13 +1,18 @@
 import { Button, ScrollView,View, Text, StyleSheet, Image,Pressable } from "react-native";
 
-export default function ContactList({contacts}){
+export default function ContactList({contacts, onChangeContact, onDeleteContact}){
 return(
     <ScrollView>
         {contacts.map((contact, index) => (
-            <Contact key={index} contact={contact.name}/>
+            <Contact 
+               key={index} 
+               contact={contact.name}
+               onChange={onChangeContact}
+               onDelete={onDeleteContact}
+            />
         ))}
     </ScrollView>
-)
+);
 }
 
 function Contact ({contact}){
