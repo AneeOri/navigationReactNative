a3a13f5e1c9309881e7f3f45e58e5ed6a132fd47
LUZ <balon.basket.90@gmail.com>
2023-11-28 22:04:34 -0600

    [+] Update Contact List Component
    Update in styles sheet

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -89,7 +89,7 @@ const styles = StyleSheet.create({
       width: 50,
       height: 50,
       borderRadius: 25,
-      marginRight: 10,
+      marginRight: 12,
     },
     row: {
       flexDirection: 'row',
